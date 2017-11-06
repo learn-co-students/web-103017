c9840fd58725a8e4406af811e1a8d652fc9aa299
Tim Campbell <timothylevicampbell@gmail.com>
2017-11-06 13:21:34 -0500

    Update seeds and DB file

--- 06_sql_review/app/models/station.rb
+++ 06_sql_review/app/models/station.rb
@@ -1,3 +1,4 @@
 class Station
-
+  # what are all the stations > station objects
+  # doing queries, find by station name > station object
 end
--- 06_sql_review/db/seeds.rb
+++ 06_sql_review/db/seeds.rb
@@ -2,6 +2,7 @@ require 'csv'
 require_relative '../config/environment'
 
 db = SQLite3::Database.new('db/subway.db')
+db.results_as_hash = true
 
 # Create the table
 db.execute(<<-SQL)
@@ -22,7 +23,33 @@ CSV.foreach('db/stations.csv') do |row|
   end
 end
 
+# Create Routes table (create table) and seed the DB (insert into)
+db.execute(<<-SQL)
+  CREATE TABLE IF NOT EXISTS routes (
+    id integer,
+    name text
+  )
+SQL
+
+CSV.foreach('db/routes.csv') do |row|
+  unless row[0] == "id"
+    db.execute("INSERT INTO routes (id, name) VALUES (#{row[0]}, \"#{row[1]}\")")
+  end
+end
+
+# Create Stops table (create table) and tseed the DB (insert into)
+db.execute(<<-SQL)
+  CREATE TABLE IF NOT EXISTS stops (
+    id integer primary key,
+    route_id integer,
+    station_id integer
+  )
+SQL
+
+CSV.foreach('db/stops.csv') do |row|
+  unless row[0] == "route_id"
+    db.execute("INSERT INTO stops (route_id, station_id) VALUES (#{row[0]}, #{row[1]})")
+  end
+end
+
 Pry.start
-
-
-# create instances of objects for each row in this table
