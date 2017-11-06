76310ef50b7a98f386763de44536b6cc9f6bff62
Tim Campbell <timothylevicampbell@gmail.com>
2017-11-06 11:35:55 -0500

    Add seed for stations

---
+++ 06_sql_review/README.md
@@ -0,0 +1,6 @@
+# SQL and Ruby
+
+## ORM
+  Object Relational Mapper
+    (relational) Store information in the database in tables
+    (object) Query the database from Ruby and convert (map) each row into an object
--- 06_sql_review/db/seeds.rb
+++ 06_sql_review/db/seeds.rb
@@ -3,6 +3,7 @@ require_relative '../config/environment'
 
 db = SQLite3::Database.new('db/subway.db')
 
+# Create the table
 db.execute(<<-SQL)
   CREATE TABLE IF NOT EXISTS stations (
     id integer,
@@ -14,6 +15,14 @@ db.execute(<<-SQL)
   )
 SQL
 
+# Seed the database
 CSV.foreach('db/stations.csv') do |row|
-  binding.pry
+  unless row[0] == "id"
+    db.execute("INSERT INTO stations (id, name, line, division, latitude, longitude) VALUES (#{row[0]}, \"#{row[1]}\", \"#{row[2]}\", \"#{row[3]}\", #{row[4]}, #{row[5]})")
+  end
 end
+
+Pry.start
+
+
+# create instances of objects for each row in this table
