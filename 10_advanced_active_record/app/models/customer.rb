0d22b2fbf3bf94d76815e439e1173894cb052e55
Tim Campbell <timothylevicampbell@gmail.com>
2017-11-14 14:05:28 -0500

    Updates to rakefile and seed file

--- 10_advanced_active_record/app/models/customer.rb
+++ 10_advanced_active_record/app/models/customer.rb
@@ -2,11 +2,9 @@ class Customer < ActiveRecord::Base
   # table name: customers
   # id, name, address
 
-  # a customer has_many purchases # list of all orders
-
-  # methods
-  # a customer has_many pending orders # subsets of orders where status = pending
-  # a customer has_many shipments # subsets of orders where status = shipped
-  # a customer has_many purchased items # through order
-  # a customer has_many top purchased brands # aggregation
+  # a customer has_many purchases             # list of all orders
+  # a customer has_many pending orders        # subsets of orders where status = pending
+  # a customer has_many shipments             # subsets of orders where status = shipped
+  # a customer has_many purchased items       # through order
+  # a customer has_many top purchased brands  # aggregation
 end
--- 10_advanced_active_record/db/seeds.rb
+++ 10_advanced_active_record/db/seeds.rb
@@ -1,3 +1,8 @@
+Product.destroy_all
+Customer.destroy_all
+Order.destroy_all
+OrderItem.destroy_all
+
 p0 = Product.create(name: 'James Steward wand', brand: 'James Steward')
 p1 = Product.create(name: 'Antioch Peverell wand', brand: 'Antioch Peverell')
 p2 = Product.create(name: 'Ollivander family wand', brand: 'Ollivander family')
@@ -17,14 +22,15 @@ c5 = Customer.create(name: 'Severus Snape', address: 'Hogwarts School of Witchcr
 c6 = Customer.create(name: 'Rubeus Hagrid', address: 'Hogwarts School of Witchcraft and Wizardry')
 c7 = Customer.create(name: 'Draco Malfoy', address: 'Hogwarts School of Witchcraft and Wizardry')
 
-o1 = Order.create(customer: c1, discount: 0.2)
-o2 = Order.create(customer: c2, discount: 0.2)
-o3 = Order.create(customer: c2, discount: 0.2)
-o4 = Order.create(customer: c3, discount: 0.2)
-o5 = Order.create(customer: c4, discount: 0.2)
+o1 = Order.create(customer: c1, discount: 0.2, status: 'pending')
+o2 = Order.create(customer: c2, discount: 0.2, status: 'pending')
+o3 = Order.create(customer: c2, discount: 0.2, status: 'confirmed')
+o4 = Order.create(customer: c3, discount: 0.2, status: 'shipped')
+o5 = Order.create(customer: c4, discount: 0.2, status: 'pending')
 
 o1.items << p1 # we don't have item class
 o1.items << p2 # there's a type mismatch
+o1.items << p2 
 o2.items << p3
 o3.items << p4
 o4.items << p5
