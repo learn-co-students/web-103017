a272758671f0b3d6d936bc6489ecb6eba42d04f6
Tim Campbell <timothylevicampbell@gmail.com>
2017-11-14 10:59:35 -0500

    Add notes

--- 10_advanced_active_record/app/models/customer.rb
+++ 10_advanced_active_record/app/models/customer.rb
@@ -2,9 +2,11 @@ class Customer < ActiveRecord::Base
   # table name: customers
   # id, name, address
 
-  # a customer has_many purchases
-  # a customer has_many pending orders
-  # a customer has_many shipments
-  # a customer has_many purchased items
-  # a customer has_many top purchased brands
+  # a customer has_many purchases # list of all orders
+
+  # methods
+  # a customer has_many pending orders # subsets of orders where status = pending
+  # a customer has_many shipments # subsets of orders where status = shipped
+  # a customer has_many purchased items # through order
+  # a customer has_many top purchased brands # aggregation
 end
--- 10_advanced_active_record/app/models/product.rb
+++ 10_advanced_active_record/app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   # table name: products
   # id, name, brand
 
-  # a product has_many related_orders
   # a product has_many order_items
+  # a product has_many related_orders, through order_items
   # a product has_many purchasing_customers
 end
