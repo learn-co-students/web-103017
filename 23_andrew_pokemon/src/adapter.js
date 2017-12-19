5fd1fa56941fa1901eb19ae94be298f818211bee
github username <andrewcohn0@gmail.com>
2017-12-19 16:49:41 -0500

    added createNewPokemon method on Adapter that we will use later

--- 23_andrew_pokemon/src/adapter.js
+++ 23_andrew_pokemon/src/adapter.js
@@ -15,4 +15,27 @@ class Adapter {
       )
       .catch(err => console.error("Something went wrong", err));
   }
+
+  static createNewPokemon(pokemonData) {
+    //creating a method to demonstrate request/response cycle; will integrate later
+    return fetch("http://localhost:3000/pokemons", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        height: 10,
+        weight: 130,
+        name: "bulbasaur",
+        abilities: ["overgrow", "chlorophyll"],
+        moves: [],
+        stats: [],
+        types: ["grass", "poison"],
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+        },
+      }),
+    });
+  }
 }
