91ce11b2c8cda50c5f3e06cb113c716a33e57986
howardbdev <howarddevennish@gmail.com>
2017-12-19 17:56:00 -0500

    final commit

--- 23_howard_pokemon/src/adapter.js
+++ 23_howard_pokemon/src/adapter.js
@@ -3,22 +3,11 @@ class Adapter {
     //class constructor
 
   }
-  // instance methods
 
   // class methods
   static getPokemon() {
     fetch('http://localhost:3000/pokemons')
       .then(response => response.json())
-      .then(json => this.makePokemons(json))
+      .then(json => Pokemon.makePokemons(json))
   }
-
-  static makePokemons(json) {
-    json.forEach(
-      function(pokemon){
-        let newPokemon = new Pokemon(pokemon)
-        newPokemon.makeDiv()
-      }
-    )
-  }
-
 }
--- 23_howard_pokemon/src/pokemon.js
+++ 23_howard_pokemon/src/pokemon.js
@@ -1,10 +1,73 @@
+// If you would like to refactor, have a look at the to-do list below :)
+
+// TO DO
+// Add filter to display the appropriate pokemon *DONE*
+// Add keyup listener to search input *DONE*
+// Add the 'flip' functionality - change display of sprite *DONE*
+// Add the CSS in
+// make allPokemon a private class variable **This might be the best place to start**
+// Remove some of the code smell that's going on below
+//  |-> refactor duplicate lines into SRP methods
+//  |-> consider breaking into a new class for handling DOM actions
+//  |-> consider different, cleaner ways of accomplishing various tasks
+
+const allPokemon = [];
 class Pokemon {
   constructor(json) {
     this.name = json.name
     this.front = json.sprites.front
     this.back = json.sprites.back
+    allPokemon.push(this)
     // make the empty div with
     // property of display none
+  }
+
+  // static class methods
+  static attachListeners() {
+    document.getElementById('pokemon-search-input').addEventListener('keyup', this.filterPokemons)
+  }
+
+  static filterPokemons(e) {
+    if (e.target.value === '') {
+      allPokemon.forEach(pokemon => pokemon.hidePoke())
+      document.getElementById('no-poke').style.display = 'block'
+    } else {
+      document.getElementById('no-poke').style.display = 'none'
+      Pokemon.displayFilteredPokemons(Pokemon.findPokemon(e.target.value))
+    }
+  }
+
+  static makePokemons(json) {
+    json.forEach(
+      function(pokemon){
+        let newPokemon = new Pokemon(pokemon)
+        newPokemon.makeDiv()
+      }
+    )
+  }
+
+  static findPokemon(query) {
+    return allPokemon.filter(pokemon => pokemon.name.includes(query))
+  }
+
+  static displayFilteredPokemons(filteredPokemons) {
+    allPokemon.forEach(pokemon => pokemon.hidePoke())
+    filteredPokemons.forEach(pokemon => pokemon.showPoke())
+    // show the 'No Pokemons' message or not
+    if (filteredPokemons.length === 0) {
+      document.getElementById('no-poke').style.display = 'block'
+    } else {
+      document.getElementById('no-poke').style.display = 'none'
+    }
+  }
+
+  toggleImage(pokemon) {
+    if (document.querySelector(`#${this.name} img`).src === this.back) {
+      document.querySelector(`#${this.name} img`).src = this.front
+    } else {
+      document.querySelector(`#${this.name} img`).src = this.back
+    }
+
   }
 
   makeDiv() {
@@ -16,12 +79,23 @@ class Pokemon {
     div.innerHTML = this.render();
     document.getElementById('pokemon-container').appendChild(div)
     div.style.display = 'none'
+    div.addEventListener('click', Pokemon.flipPoke)
   }
 
+  static flipPoke(e) {
+    if (e.target.id.includes('flip')) {
+      let pokemon = allPokemon.find(p => p.name === e.target.dataset.id)
+      pokemon.toggleImage()
+    }
+  }
+
+  // show a pokemon
   showPoke() {
+    document.querySelector(`#${this.name} img`).src = this.front
     document.getElementById(this.name).style.display = 'block'
   }
 
+  // hide a pokemon
   hidePoke() {
     document.getElementById(this.name).style.display = 'none'
   }
@@ -32,56 +106,8 @@ class Pokemon {
       `<div>
         <h1>${this.name}</h1>
         <img src=${this.front}>
-        <p>flip card</p>
+        <p data-id=${this.name} data-brendan="Brendan" id="p-flip-${this.name}">flip card</p>
       </div>`
     )
-  }
-
-
-
-}
-
-
-const pokes = {
-  "height": 10,
-  "weight": 130,
-  "order": 2,
-  "name": "ivysaur",
-  "abilities": [
-    "overgrow",
-    "chlorophyll"],
-  "moves": [],
-  "stats": [
-    { "value": 80,
-      "name": "special-defense"
-    },
-    {
-      "value": 80,
-      "name": "special-attack"
-    },
-    {
-      "value": 63,
-      "name": "defense"
-    },
-    {
-      "value": 62,
-      "name": "attack"
-    },
-    {
-      "value": 60,
-      "name": "speed"
-    },
-    {
-      "value": 60,
-      "name": "hp"
-    }
-  ],
-  "types": [
-    "grass",
-    "poison"
-  ],
-  "sprites": {
-    "front": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
-    "back": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/2.png"
   }
 }
