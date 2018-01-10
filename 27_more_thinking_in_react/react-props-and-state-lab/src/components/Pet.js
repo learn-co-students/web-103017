44254ecdb377493665ea893a0cfb3674766c4992
alexgriff <alexalexgriffith@gmail.com>
2018-01-10 14:02:04 -0500

    ready for lecture for real

--- 27_more_thinking_in_react/react-props-and-state-lab/src/components/Pet.js
+++ 27_more_thinking_in_react/react-props-and-state-lab/src/components/Pet.js
@@ -6,35 +6,20 @@ class Pet extends React.Component {
   }
 
   render() {
-    const { pet } = this.props;
-
     return (
       <div className="card">
         <div className="content">
-          <a className="header">
-            {pet.name} {pet.gender === 'female' ? '♀' : '♂'}
-          </a>
+          <a className="header">{/* name,  gender '♀' : '♂' */}</a>
           <div className="meta">
-            <span className="date">{pet.type}</span>
+            <span className="date">pet type</span>
           </div>
           <div className="description">
-            <p>Age: {pet.age}</p>
-            <p>Weight: {pet.weight}</p>
+            <p>Age: </p>
+            <p>Weight: </p>
           </div>
         </div>
         <div className="extra content">
-          {pet.isAdopted ? (
-            <button className="ui disabled button">Already adopted</button>
-          ) : (
-            <button
-              onClick={() => {
-                this.props.onAdoptClick(pet);
-              }}
-              className="ui primary button"
-            >
-              Adopt pet
-            </button>
-          )}
+          <button className="ui primary button">Adopt pet</button>
         </div>
       </div>
     );
