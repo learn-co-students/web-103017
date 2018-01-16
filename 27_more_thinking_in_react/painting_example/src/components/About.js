0aad30d6540a4431fdc8d485e546ede27ca99ed1
nikymorg <nicole.morgan@gmail.com>
2018-01-15 21:45:00 -0500

    add about and login components

---
+++ 27_more_thinking_in_react/painting_example/src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>Alex is good at art.</h2>
+      <h3>He painted this:</h3>
+      <img
+        src='http://images.mentalfloss.com/sites/default/files/styles/mf_image_3x2/public/11154581_10152936784158845_7259455243370641749_o_1.jpg?itok=k6ggQkMa&resize=1100x740'
+        alt='alex-art'
+        />
+    </div>
+  );
+};
+
+export default About;
--- 27_more_thinking_in_react/painting_example/src/components/App.js
+++ 27_more_thinking_in_react/painting_example/src/components/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Navbar from './Navbar';
 import PaintingContainer from './PaintingContainer';
+import About from './About';
+import Login from './Login';
+
 
 const App = () => {
   return (
     <div>
       <Navbar icon="paint brush" title="Painterest" description="our app" />
+      { /* <About /> */}
+      { /* <Login /> */}
       <PaintingContainer />
     </div>
   );
