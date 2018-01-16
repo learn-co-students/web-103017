28782888df4fd978ff588c9aeaeedbe3265481fa
github username <andrewcohn0@gmail.com>
2018-01-16 09:36:54 -0500

    formatting

--- 29_youtube_review/andrew/src/YouTubeContainer.js
+++ 29_youtube_review/andrew/src/YouTubeContainer.js
@@ -5,31 +5,31 @@ import API_KEY from "./keys";
 
 // `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${API_KEY}&q=${searchTerm}&type=video`
 class YouTubeContainer extends React.Component {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.state = {
-			term: "puppies",
-			videos: [],
-			selectedVideo: null
-		};
-	}
+    this.state = {
+      term: "puppies",
+      videos: [],
+      selectedVideo: null,
+    };
+  }
 
-	render() {
-		const searchTerm = "dog";
-		fetch(
-			`https://www.googleapis.com/youtube/v3/search?part=snippet&key=${API_KEY}&q=${searchTerm}&type=video`
-		)
-			.then(resp => resp.json())
-			.then(console.log);
+  render() {
+    const searchTerm = "dog";
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${API_KEY}&q=${searchTerm}&type=video`
+    )
+      .then(resp => resp.json())
+      .then(console.log);
 
-		return (
-			<div className="ui grid container">
-				<h1>HI</h1>
-				{/*TODO: create some react components to build out the rest of this app*/}
-			</div>
-		);
-	}
+    return (
+      <div className="ui grid container">
+        <h1>HI</h1>
+        {/*TODO: create some react components to build out the rest of this app*/}
+      </div>
+    );
+  }
 }
 
 export default YouTubeContainer;
